2d672b2edbcc8fe503dbe056786c6e0aad4c0eee
Matheus Assunção <matheus.tba@hotmail.com>
2021-04-24 04:20:18 -0300

    aula 24/04/21

--- expo/meuApp/App_29_vector_icons.js
+++ expo/meuApp/App_29_vector_icons.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useMemo, useRef} from "react";
 import { View, Text, StyleSheet, Animated, TouchableOpacity, TextInput, SafeAreaView, Keyboard } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
 
 // Biblioteca de consulta dos ícones disponíveis no expo: https://icons.expo.fyi/
 
@@ -14,7 +14,14 @@ export default function App() {
 				<Text style={{color: '#ccc', fontSize: 35, fontWeight: 'bold'}}>
 					Hello World
 				</Text>
-				<Ionicons name="checkmark-circle" size={62} color="green" />
+				<Ionicons name="checkmark-circle" size={35} color="green" />
+
+				<FontAwesome name="wechat" size={35} color="green" />
+
+				<TouchableOpacity style={styles.btn}>
+					<MaterialCommunityIcons name="chat" size={20} color="white" />
+					<Text style={{color: 'white', fontSize: 20}}>Abrir chat</Text>
+				</TouchableOpacity>
 			</View>
 		</SafeAreaView>
 	);
@@ -26,4 +33,7 @@ const styles = StyleSheet.create({
 		padding: 10,
 		paddingTop: Platform.OS === "android" ? 35 : 0,
 	},
+	btn: {
+		flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', width: 150, padding: 5, backgroundColor: 'green', borderRadius: 5
+	}
 });
